0a2844ca47e79ddb0f93540635b24d30f39fab98
DroidDevelopment <johnship2876@gmail.com>
2024-10-19 23:41:32 -0400

    Undo previous commit.

--- lib/discordrb/api/interaction.rb
+++ lib/discordrb/api/interaction.rb
@@ -6,7 +6,7 @@ module Discordrb::API::Interaction
 
   # Respond to an interaction.
   # https://discord.com/developers/docs/interactions/slash-commands#create-interaction-response
-  def create_interaction_response(interaction_token, interaction_id, type, content = nil, tts = nil, embeds = nil, allowed_mentions = nil, flags = nil, components = nil, choices = [])
+  def create_interaction_response(interaction_token, interaction_id, type, content = nil, tts = nil, embeds = nil, allowed_mentions = nil, flags = nil, components = nil, choices = nil)
     data = { tts: tts, content: content, embeds: embeds, allowed_mentions: allowed_mentions, flags: flags, components: components, choices: choices }.compact
 
     Discordrb::API.request(
