c7c5d3d8bed26afd970e478f9f7b2feae8e7f617
DroidDevelopment <johnship2876@gmail.com>
2024-10-20 00:27:45 -0400

    New changes.

--- lib/discordrb/events/interactions.rb
+++ lib/discordrb/events/interactions.rb
@@ -397,8 +397,9 @@ module Discordrb::Events
   class AutocompleteEvent < ApplicationCommandEvent
     # Return autocomplete options to a user as they are typing.
     # @param options [Array] Array of options to return.
-    def initialize
-      super(data, bot)
+    def initialize(data, bot)
+      super
+      
       @options = []
     end
 
