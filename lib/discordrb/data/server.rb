77f399988dd6d6b6804a6529a7326900f2820ddf
Droid <johnship2876@gmail.com>
2024-11-03 19:43:02 -0500

    Update server.rb

--- lib/discordrb/data/server.rb
+++ lib/discordrb/data/server.rb
@@ -611,6 +611,11 @@ module Discordrb
       end
     end
 
+    def emoji_limit?
+      return true if @emoji.keys.count >= max_emoji
+      return false if @emoji.keys.count < max_emoji
+    end 
+
     # Searches a server for members that matches a username or a nickname.
     # @param name [String] The username or nickname to search for.
     # @param limit [Integer] The maximum number of members between 1-1000 to return. Returns 1 member by default.
