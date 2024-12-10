0dcdd3152b27734be7efcc87af0ee6015f53d377
DroidDevelopment <johnship2876@gmail.com>
2024-12-10 17:37:03 -0500

    Minor fix.

--- lib/discordrb/data/channel.rb
+++ lib/discordrb/data/channel.rb
@@ -500,7 +500,7 @@ module Discordrb
 
       yield(poll, view) if block_given?
 
-      send_message(message, tts, embed, attachments, allowed_mentions, message_reference, components || view.to_a, poll || builder.to_h)
+      send_message(message, tts, embed, attachments, allowed_mentions, message_reference, components || view.to_a, poll || poll.to_h)
     end
 
     # Sends multiple messages to a channel
