543f077a05b16dd24b4359c6724c720dfbcd3a85
Droid <johnship2876@gmail.com>
2025-02-20 19:35:20 -0500

    Debug Stuff

--- lib/discordrb/api/interaction.rb
+++ lib/discordrb/api/interaction.rb
@@ -7,6 +7,7 @@ module Discordrb::API::Interaction
   # Respond to an interaction.
   # https://discord.com/developers/docs/interactions/slash-commands#create-interaction-response
   def create_interaction_response(interaction_token, interaction_id, type, content = nil, tts = nil, embeds = nil, allowed_mentions = nil, flags = nil, components = nil)
+    print(components)
     data = { tts: tts, content: content, embeds: embeds, allowed_mentions: allowed_mentions, flags: flags, components: components }.compact
 
     Discordrb::API.request(
