a09c3f292a742b2fcd2d004d93c78b0e90e8779e
Droid <johnship2876@gmail.com>
2025-06-30 17:48:33 -0400

    add basic auto-moderation gateway events

---
+++ lib/discordrb/data/auto_moderation.rb
@@ -0,0 +1,319 @@
+# frozen_string_literal: true
+
+module Discordrb
+  # An automatic moderator for a server that can trigger based on a set critera.
+  class AutoModRule
+    include IDObject
+
+    # Map of event types.
+    EVENT_TYPES = {
+      message_send: 1,
+      member_update: 2
+    }.freeze
+
+    # @return [String] the name of this automod rule.
+    attr_reader :name
+
+    # @return [Integer] the event type of this automod rule.
+    # @see {EVENT_TYPES}
+    attr_reader :event_type
+
+    # @return [Trigger] how this automod rule can be triggered.
+    attr_reader :trigger
+
+    # @return [Array<Action>] the actions that will execute when this automod rule is triggered.
+    attr_reader :actions
+
+    # @return [true, false] whether this automod rule is enabled or not.
+    attr_reader :enabled
+    alias_method :enabled?, :enabled
+
+    # @return [Array<Role>] the roles that are ignored by this automod rule.
+    attr_reader :exempt_roles
+
+    # @return [Array<Channel>] the channels that are ignored by this automod rule.
+    attr_reader :exempt_channels
+
+    # @!visibility private
+    def initialize(data, bot, server = nil)
+      @bot = bot
+      @server = server
+      @id = data['id'].to_i
+      @server_id = data['guild_id'].to_i
+      @creator_id = data['creator_id'].to_i
+      from_other(data)
+    end
+
+    # @return [User] the user who was responsible for the creation of this automod rule.
+    def creator
+      @bot.user(@creator_id)
+    end
+
+    # @return [Server] the server this automod rule belongs from.
+    def server
+      @server ||= @bot.server(@server_id)
+    end
+
+    # Check if something is exempt from this automod rule.
+    # @param other [Role, Channel, Member] The thing to check for exemption.
+    # @return [true, false] Whether the provided object is exempt or not.
+    def exempt?(other)
+      case other
+      when Discordrb::Member
+        other.permission?(:manage_server) || other.roles.intersect?(exempt_roles)
+      when Role, Channel
+        exempt_roles.any?(other) || exempt_channels.any?(other)
+      else
+        raise ArgumentError, "Unsupported type: #{other.class}"
+      end
+    end
+
+    # Set the name of this automod rule.
+    # @param name [String] the new name of this automod rule.
+    def name=(name)
+      update_data(name: name)
+    end
+
+    # Set whether this automod rule is enabled or not.
+    # @param enabled [true, false] whether this rule is enabled or not.
+    def enabled=(enabled)
+      update_data(enabled: enabled)
+    end
+
+    # Set the event type of this automod rule.
+    # @param event_type [Symbol, Integer] the new event type of this automod rule.
+    def event_type=(type)
+      update_data(event_type: EVENT_TYPES[type] || type)
+    end
+
+    # Set the roles that are ignored by this automod rule.
+    # @param exempt_roles [Array<Role, Integer, String>] the new exmpet roles.
+    def exempt_roles=(exempt_roles)
+      update_data(exempt_roles: exempt_roles.map(&:resolve_id))
+    end
+
+    # Set the channels that are ignored by this automod rule.
+    # @param exempt_channels [Array<Channel, Integer, String>] the new exempt channels.
+    def exempt_channels=(exempt_channels)
+      update_data(exempt_channels: exempt_channels.map(&:resolve_id))
+    end
+
+    # Delete this auto moderation rule.
+    # @param reason [String, nil] the reason for deleting this automod rule.
+    def delete(reason = nil)
+      API::Server.delete_automod_rule(@bot.token, @server.id, @id, reason)
+      @server.delete_automod_rule(@id)
+    end
+
+    # @!visibility private
+    def from_other(new_data)
+      @name = new_data['name']
+      @enabled = new_data['enabled']
+      @event_type = new_data['event_type']
+      @actions = new_data['actions'].map { |action| Action.new(action, @bot) }
+      @exempt_roles = new_data['exempt_roles'].map { |role_id| @server.role(role_id) }
+      @exempt_channels = new_data['exempt_channels'].map { |chan_id| @bot.channel(chan_id) }
+      @trigger = Trigger.new(new_data['trigger_metadata'].merge({ 'type' => new_data['trigger_type'] }), self, @bot)
+    end
+
+    # @!visibility private
+    def update_data(new_data)
+      from_other(JSON.parse(API::Server.update_automod_rule(@bot.token, @server.id, @id,
+                                                            new_data[:name], new_data[:event_type],
+                                                            new_data[:trigger], new_data[:actions],
+                                                            new_data[:enabled], new_data[:exempt_roles],
+                                                            new_data[:exempt_channels], new_data[:reason])))
+    end
+
+    # Information used to determine when a rule should be triggered.
+    class Trigger
+      # Map of trigger types.
+      TYPES = {
+        keyword: 1,
+        harmful_link: 2,
+        spam: 3,
+        keyword_preset: 4,
+        mention_spam: 5,
+        member_profile: 6
+      }.freeze
+
+      # Map of keyword preset types.
+      PRESET_TYPES = {
+        profanity: 1,
+        sexual_content: 2,
+        slurs: 3
+      }.freeze
+
+      # @return [Integer] the type of this automod trigger.
+      # @see {TYPES}
+      attr_reader :type
+
+      # @return [Array<String>] substrings that can trigger the automod rule.
+      attr_reader :keyword_filter
+
+      # @return [Array<String>] regex patterns that when matched can trigger this rule.
+      attr_reader :regex_patterns
+
+      # @return [Array<Integer>] set of word types that can trigger the automod rule.
+      # @see {PRESET_TYPES}
+      attr_reader :keyword_presets
+
+      # @return [Array<String>] substrings that should not trigger the automod rule.
+      attr_reader :exempt_keywords
+
+      # @return [Integer] total number of unique role and user mentions allowed per message.
+      attr_reader :mention_limit
+
+      # @return [true, false] whether the automod rule will automatically detect mention raids.
+      attr_reader :mention_raid_protection
+      alias_method :mention_raid_protection?, :mention_raid_protection
+
+      # @!visibility private
+      def initialize(data, rule, bot)
+        @bot = bot
+        @rule = rule
+        @type = data['type']
+        @keyword_filter = data['keyword_filter'] || []
+        @regex_patterns = data['regex_patterns'] || []
+        @keyword_presets = data['presets'] || []
+        @exempt_keywords = data['allow_list'] || []
+        @mention_limit = data['mention_total_limit'] || 0
+        @mention_raid_protection = data['mention_raid_protection_enabled'] || false
+      end
+
+      # Set the substrings that should not trigger the automod rule.
+      # @param exempt_keywords [Array<String>]
+      def exempt_keywords=(exempt_keywords)
+        @rule.update_data(trigger: to_h.merge(allow_list: exempt_keywords))
+      end
+
+      # Set the regex patterns that can trigger the automod rule.
+      # @param regex_patterns [Array<String>]
+      def regex_patterns=(regex_patterns)
+        @rule.update_data(trigger: to_h.merge(regex_patterns: regex_patterns))
+      end
+
+      # Set the substrings that can trigger the automod rule.
+      # @param keyword_filter [Array<String>]
+      def keyword_filter=(keyword_filter)
+        @rule.update_data(trigger: to_h.merge(keyword_filter: keyword_filter))
+      end
+
+      # Set the maximum amount of unique mentions allowed for the rule.
+      # @param mention_limit [Integer]
+      def mention_limit=(mention_limit)
+        @rule.update_data(trigger: to_h.merge(mention_total_limit: mention_limit))
+      end
+
+      # Set whether mention raid protection is enabled for the rule or not.
+      # @param mention_raid_protection [true, false]
+      def mention_raid_protection=(raid_protection)
+        @rule.update_data(trigger: to_h.merge(mention_raid_protection_enabled: raid_protection))
+      end
+
+      # Set the keyword presets for this rule.
+      # @param keyword_presets [Array<Integer, Symbol>]
+      def keyword_presets=(presets)
+        presets.map! { |type| PRESET_TYPES[type] || type }
+
+        @rule.update_data(trigger: to_h.merge(presets: presets))
+      end
+
+      # @!method keyword?
+      #   @return [true, false] If this rule can be triggered due to a user defined keyword.
+      # @!method harmful_link?
+      #   @return [true, false] If this rule can be triggered when a harmful link is posted.
+      # @!method spam?
+      #   @return [true, false] If this rule can be triggered due to spam content.
+      # @!method keyword_preset?
+      #   @return [true, false] If this rule can be triggered due to a pre-defined keyword preset.
+      # @!method mention_spam?
+      #   @return [true, false] If this rule can be triggered due to mention spam.
+      # @!method member_profile?
+      #   @return [true, false] If this rule can be triggered due to content in a member's profile.
+      TYPES.each do |name, value|
+        define_method("#{name}?") do
+          @type == value
+        end
+      end
+
+      # @!visibility private
+      def to_h
+        {
+          keyword_filter: keyword_filter,
+          regex_patterns: regex_patterns,
+          presets: keyword_presets,
+          allow_list: exempt_keywords,
+          mention_total_limit: mention_limit,
+          mention_raid_protection_enabled: mention_raid_protection
+        }.compact
+      end
+    end
+
+    # An action that will execute whenever an automod rule is triggered.
+    class Action
+      # Map of action types.
+      TYPES = {
+        block_message: 1,
+        send_alert_message: 2,
+        timeout: 3,
+        block_member_interaction: 4
+      }.freeze
+
+      # @return [Integer] the type of action that will execute.
+      # @see {TYPES}
+      attr_reader :type
+
+      # @return [Integer, nil] the timeout duration for this action.
+      # @note Only returns a non-nil value when the action type is 3 (timeout).
+      attr_reader :timeout_duration
+
+      # @return [String, nil] explanation shown to members whenever their message is blocked.
+      # @note Only returns a non-nil value when the action type is 1 (block_message).
+      attr_reader :custom_message
+
+      # @!visibility private
+      def initialize(data, bot)
+        @bot = bot
+        @type = data['type']
+        @channel_id = data['metadata']['channel_id']
+        @custom_message = data['metadata']['custom_message']
+        @timeout_duration = data['metadata']['timeout_duration']
+      end
+
+      # @return [Channel, nil] The channel to which user content should be logged.
+      # @note Only returns a non-nil value when the action type is 2 (send_alert_message).
+      def alert_channel
+        @bot.channel(@channel_id) if @channel_id
+      end
+
+      alias_method :channel, :alert_channel
+
+      # @!method block_message?
+      #   @return [true, false] whether the type of this action is 1 (block_message).
+      # @!method send_alert_message?
+      #   @return [true, false] whether the type of this action is 2 (send_alert_message).
+      # @!method timeout?
+      #   @return [true, false] whether the type of this action is 3 (timeout).
+      # @!method block_member_interaction?
+      #   @return [true, false] whether the type of this action is 4 (block_member_interaction).
+      TYPES.each do |name, value|
+        define_method("#{name}?") do
+          @type == value
+        end
+      end
+
+      # @!visibility private
+      def to_h
+        {
+          type: @type,
+          metadata: {
+            custom_message: @custom_message,
+            channel_id: @channel_id,
+            timeout_duration: @timeout_duration
+          }.compact
+        }.reject(&:empty?)
+      end
+    end
+  end
+end
---
+++ lib/discordrb/events/auto_moderation.rb
@@ -0,0 +1,203 @@
+# frozen_string_literal: true
+
+module Discordrb::Events
+  # Generic subclass for auto moderation rule events (create/update/delete)
+  class AutoModRuleEvent < Event
+    # @return [AutoModRule] the auto moderation rule in question.
+    attr_reader :automod_rule
+
+    # @return [Server] the server the automod rule is from.
+    attr_reader :server
+
+    # @!visibility private
+    def initialize(data, bot)
+      @bot = bot
+      @server = bot.server(data['guild_id'].to_i)
+      @automod_rule = @server.automod_rule(data['id'].to_i)
+    end
+  end
+
+  # This event is raised whenever an automod rule is triggered.
+  class AutoModActionEvent < Event
+    # @!visibility private
+    attr_reader :server_id, :rule_id, :user_id, :channel_id
+
+    # @return [AutoModRule::Action] the action which was taken.
+    attr_reader :action
+
+    # @return [Integer] Trigger type of the rule which was triggered.
+    attr_reader :trigger_type
+
+    # @return [String] User generated content that triggered the rule.
+    attr_reader :content
+
+    # @return [String, nil] Word or phrase that triggered the rule.
+    attr_reader :matched_keyword
+
+    # @return [String, nil] The substring in content that triggered the rule.
+    attr_reader :matched_content
+
+    # @!visibility private
+    def initialize(data, bot)
+      @bot = bot
+      @server_id = data['guild_id'].to_i
+      @rule_id = data['rule_id'].to_i
+      @user_id = data['user_id'].to_i
+      @channel_id = data['channel_id']&.to_i
+      @message_id = data['message_id']&.to_i
+      @alert_message_id = data['alert_system_message_id']&.to_i
+
+      @action = Discordrb::AutoModRule::Action.new(data['action'], bot)
+      @trigger_type = data['trigger_type']
+      @content = data['content']
+      @matched_keyword = data['matched_keyword']
+      @matched_content = data['matched_content']
+    end
+
+    # @return [Server] the server this event originates from.
+    def server
+      @bot.server(@server_id)
+    end
+
+    # @return [Channel] channel in which user content was posted.
+    def channel
+      @bot.channel(@channel_id) if @channel_id
+    end
+
+    # @return [AutoModRule] the rule that was triggered for this event.
+    def automod_rule
+      @automod_rule ||= server.automod_rule(@rule_id)
+    end
+
+    # @return [User, Member] the user or member which generated the content that triggered the rule.
+    def user
+      server.member(@user_id) || @bot.user(@user_id)
+    end
+
+    alias_method :member, :user
+
+    # @return [Message, nil] message which contants user generated content that triggered this rule.
+    def message
+      @message_id ? (@message ||= channel.load_message(@message_id)) : nil
+    end
+
+    # @return [Message, nil] System auto moderation message posted as a result of this action.
+    def alert_message
+      @alert_channel ||= automod_rule.actions.find(&:send_alert_message?)&.channel
+
+      @alert_channel&.load_message(@alert_message_id) if @alert_message_id
+    end
+  end
+
+  # Event handler for generic auto-moderation rule events.
+  class AutoModRuleEventHandler < EventHandler
+    # @!visibility private
+    def matches?(event)
+      return false unless event.is_a? AutoModRuleEvent
+
+      [
+        matches_all(@attributes[:server], event.server) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:automod_rule], event.automod_rule) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:creator], event.automod_rule.creator) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:event_type], event.automod_rule.event_type) do |a, e|
+          case a
+          when Symbol, String
+            e == Discordrb::AutomodRule::EVENT_TYPES[a.to_sym]
+          when Integer
+            e == a
+          end
+        end,
+
+        matches_all(@attributes[:trigger_type], event.automod_rule.trigger_type) do |a, e|
+          case a
+          when Symbol, String
+            e == Discordrb::AutomodRule::Trigger::TYPES[a.to_sym]
+          when Integer
+            e == a
+          end
+        end
+      ].reduce(true, &:&)
+    end
+  end
+
+  # Event handler for auto-moderation rule execution events.
+  class AutoModActionEventHandler < EventHandler
+    # @!visibility private
+    def matches?(event)
+      return false unless event.is_a? AutoModActionEvent
+
+      [
+
+        matches_all(@attributes[:user], event.user_id) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:server], event.server_id) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:channel], event.channel_id) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:automod_rule], event.rule_id) do |a, e|
+          a.resolve_id == e
+        end,
+
+        matches_all(@attributes[:content], event.content) do |a, e|
+          case a
+          when String
+            e == a
+          when Regexp
+            a.match?(e) if e
+          end
+        end,
+
+        matches_all(@attributes[:trigger_type], event.trigger_type) do |a, e|
+          case a
+          when Symbol, String
+            e == Discordrb::AutomodRule::Trigger::TYPES[a.to_sym]
+          when Integer
+            e == a
+          end
+        end,
+
+        matches_all(@attributes[:matched_content], event.matched_content) do |a, e|
+          case a
+          when String
+            e == a
+          when Regexp
+            a.match?(e) if e
+          end
+        end,
+
+        matches_all(@attributes[:matched_keyword], event.matched_keyword) do |a, e|
+          case a
+          when String
+            e == a
+          when Regexp
+            a.match?(e) if e
+          end
+        end,
+
+        matches_all(@attributes[:event_type], event.automod_rule.event_type) do |a, e|
+          case a
+          when Symbol, String
+            e == Discordrb::AutomodRule::EVENT_TYPES[a.to_sym]
+          when Integer
+            e == a
+          end
+        end
+      ].reduce(true, &:&)
+    end
+  end
+end
