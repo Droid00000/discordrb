0688920a672a7f0ed72fc74ee1cdf9058c1bb188
Droid <johnship2876@gmail.com>
2025-07-16 17:50:31 -0400

    intents

--- lib/discordrb.rb
+++ lib/discordrb.rb
@@ -32,7 +32,9 @@ module Discordrb
     server_message_typing: 1 << 11,
     direct_messages: 1 << 12,
     direct_message_reactions: 1 << 13,
-    direct_message_typing: 1 << 14
+    direct_message_typing: 1 << 14,
+    server_auto_moderation: 1 << 20,
+    server_auto_moderation_exection: 1 << 21
   }.freeze
 
   # All available intents
